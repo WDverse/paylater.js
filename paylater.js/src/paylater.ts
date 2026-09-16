b21f433ff7420283690e2f2f7c35f8ef74ece0f0
Emmanuel Appiagyei <emmanuelappiagyei@cmail.carleton.ca>
2026-09-16 02:09:00 -0400

    format currency using browser locale

--- paylater.js/src/paylater.ts
+++ paylater.js/src/paylater.ts
@@ -2,13 +2,17 @@ import { splitPayment } from "./math.ts";
 const priceEls = document.querySelectorAll<HTMLElement>('[data-paylater-amount]');
 
 priceEls.forEach (el => {
-  const amt =  parseFloat(el.dataset.paylaterAmount ?? '0');
+  const amt = parseFloat(el.dataset.paylaterAmount ?? '0'); // default amt to 0 if undefined
   const amtInCents = Math.round(amt * 100);
   const perPayment = splitPayment(amtInCents, 4);
-  const displayedPrice = (perPayment/100).toFixed(2);
+
+  const formatter = new Intl.NumberFormat(navigator.language, { //use browser locale to handle currency formatting
+    style: 'currency',
+    currency: el.dataset.paylaterCurrency ?? 'USD', // default to USD if undefined
+  });
 
   const msgEl = document.createElement('p');
   msgEl.className = 'paylater-message';
-  msgEl.textContent = `or 4 payments of $${displayedPrice}`;
+  msgEl.textContent = `or 4 payments of ${formatter.format(perPayment / 100)}`;
   el.insertAdjacentElement('afterend', msgEl)
 })
