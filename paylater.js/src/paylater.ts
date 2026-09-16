4a5e03657b4699531830f369cccdf564d9b542a7
Emmanuel Appiagyei <emmanuelappiagyei@cmail.carleton.ca>
2026-09-15 23:40:05 -0400

    render payment plan for each price

--- paylater.js/src/paylater.ts
+++ paylater.js/src/paylater.ts
@@ -1,1 +1,10 @@
-const priceEls = document.querySelectorAll('[data-paylater-amount]')
+const priceEls = document.querySelectorAll<HTMLElement>('[data-paylater-amount]');
+
+priceEls.forEach (el => {
+  const amt =  parseFloat(el.dataset.paylaterAmount ?? '0');
+  const perPayment = (amt / 4).toFixed(2);
+  const msgEl = document.createElement('p');
+  msgEl.className = 'paylater-message';
+  msgEl.textContent = `or 4 payments of $${perPayment}`;
+  el.insertAdjacentElement('afterend', msgEl)
+})
